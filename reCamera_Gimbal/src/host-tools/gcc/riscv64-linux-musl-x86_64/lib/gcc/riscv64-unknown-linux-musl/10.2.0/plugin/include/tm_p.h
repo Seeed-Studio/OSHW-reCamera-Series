aad08dc8602e455e1f978bcba10b1ce3384c46a4
Parker Hu <hbzjkhhz@163.com>
2025-03-17 17:45:58 +0800

    [fix] Modify the Gimbal readme

--- reCamera_Gimbal/src/host-tools/gcc/riscv64-linux-musl-x86_64/lib/gcc/riscv64-unknown-linux-musl/10.2.0/plugin/include/tm_p.h
+++
@@ -1,8 +0,0 @@
-#ifndef GCC_TM_P_H
-#define GCC_TM_P_H
-#ifdef IN_GCC
-# include "config/riscv/riscv-protos.h"
-# include "config/linux-protos.h"
-# include "tm-preds.h"
-#endif
-#endif /* GCC_TM_P_H */
